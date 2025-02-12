3b49573ddfea3a18d6028d63cb487dcb88ebaeb0
victor <victorvobis@web.de>
2025-02-12 09:51:38 +0100

    bless

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Download alpine iso
-wget https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/x86_64/alpine-standard-3.19.0-x86_64.iso -O alpine/alpine.iso
-
-# Create qemu volume
-qemu-img create -f qcow2 alpine.qcow2 2
