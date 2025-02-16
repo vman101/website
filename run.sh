cfd8c994205e8f0e0d3c9983382c139deec029c3
Victor Vobis <vvobis@c4r1p5.42vienna.com>
2025-02-16 19:07:18 +0100

    fixed docker

--- run.sh
+++ run.sh
@@ -5,13 +5,18 @@ set -e
 # Compile the minishell project
 make -C minishell/minishell_src
 
+# rebuild website
+cd vvsite && cargo leptos build
+cd ..
+
 # stop containers if running
 docker compose down -v
 
 # remove volumes
 docker volume prune -f
-docker network rm website_website_net -f
-docker rmi website-nginx
+
+docker rmi website-website website-minishell website-nginx -f
+
 
 # start the containers
 docker compose up
--- vvsite/src/website/client/pages/minishell.rs
+++ vvsite/src/website/client/pages/minishell.rs
@@ -8,7 +8,7 @@ pub fn MinishellPage() -> impl IntoView{
             <div class="flex h-[8vh] place-content-stretch">
                 <h2 class="text-4xl py-8">"The (mini) Shell"</h2>
             </div>
-            <iframe src="http://localhost:19001/minishell" class="place-self-center" width="800" height="600" />
+            <iframe src="http://127.0.0.1:19001/minishell/vnc" class="place-self-center" width="800" height="600" />
         </div>
     }
 }
